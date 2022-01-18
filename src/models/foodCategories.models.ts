b441889b48635114984dce02d4f30efc97cc68d0
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-01-18 18:43:29 +0200

    RBH-4 | Add foodCategories model and createFoodCategoryEntry

---
+++ src/models/foodCategories.models.ts
@@ -0,0 +1,15 @@
+import mongoose, { Document } from 'mongoose';
+
+export interface FoodCategoriesModel extends Document {
+    id: number;
+    name: string;
+}
+
+const schema = new mongoose.Schema({
+    id: Number,
+    name: { type: String, required: true },
+});
+
+const foodCategoriesModel = mongoose.model<FoodCategoriesModel>('foodCategoriesModel', schema);
+
+export default foodCategoriesModel;
