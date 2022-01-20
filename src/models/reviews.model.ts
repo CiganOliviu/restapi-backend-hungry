5f322847568f63ccaaf4aefe7e120385c53f6a87
Ionita Catalin <catalin_ionita04@yahoo.com>
2022-01-20 16:15:48 +0200

    RBH-5 | Create table reviews

---
+++ src/models/reviews.model.ts
@@ -0,0 +1,28 @@
+import mongoose, {Document, Schema} from 'mongoose';
+import { UsersModel } from "./users.model";
+
+export interface ReviewsModel extends Document {
+    id: number;
+    number_of_stars: number;
+    comment: string;
+    posted_on: Date;
+    edited_at: Date;
+    author: UsersModel;
+}
+
+const schema = new mongoose.Schema({
+    id: Number,
+    numberOfStars: { type: Number, required:true },
+    comment: { type: String, required:true },
+    postedOn: { type: Date, required:true },
+    editedAt: { type: Date, required:true },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "UsersModel",
+        required:true
+    }
+});
+
+const reviewsModel = mongoose.model<ReviewsModel & mongoose.Document>('reviewsModel', schema);
+
+export default reviewsModel;
