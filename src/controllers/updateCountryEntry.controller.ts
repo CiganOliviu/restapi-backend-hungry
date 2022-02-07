feff14e77527ea8cd23ab5f889c38662d3017650
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-02-07 23:44:16 +0200

    RBH-9 | Update routes.ts

--- src/controllers/updateCountryEntry.controller.ts
+++ src/controllers/updateCountryEntry.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express"
 import countriesModel from "../models/countries.model";
 
-export async function updateCountriesEntryController(request: Request, response: Response) {
+export async function updateCountryEntryController(request: Request, response: Response) {
     // implement me
 }
