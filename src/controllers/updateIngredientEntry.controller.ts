45745bd4317c7bc507e54723a8828332e4c66dea
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-02-08 19:10:34 +0200

    RBH-9 | setup updateIngredientController

--- src/controllers/updateIngredientEntry.controller.ts
+++ src/controllers/updateIngredientEntry.controller.ts
@@ -1,6 +1,16 @@
 import { Request, Response } from "express";
-import IngredientsModel from "../models/ingredients.model";
+import ingredientsModel from "../models/ingredients.model";
 
 export async function updateIngredientEntryController(request: Request, response: Response) {
-    // implement me
+    const { name, nameToUpdate } = request.body;
+    const filterData = { name: name };
+    const updateData = { name: nameToUpdate }
+
+    const data = await ingredientsModel.findOneAndUpdate(filterData, updateData);
+
+    if (data) {
+        console.log(`Data ${data} was updated!`);
+    }
+
+    return response.status(200).json();
 }
