36b20635237b5bfa498d83b6f319ce39d87cb160
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-02-10 23:51:51 +0200

    RBH-9 | Create updateSchemasEntry

--- src/controllers/updateSchemaEntry.controller.ts
+++ src/controllers/updateSchemaEntry.controller.ts
@@ -2,5 +2,15 @@ import { Request, Response } from "express";
 import schemasModel from "../models/schemas.model";
 
 export async function updateSchemaEntryController(request: Request, response: Response) {
-    // implement me
+    const { name, nameToUpdate } = request.body;
+    const filterData = { name: name };
+    const updateData = { name: nameToUpdate }
+
+    const data = await schemasModel.findOneAndUpdate(filterData, updateData);
+
+    if (data) {
+        console.log(`Data ${data} was updated!`);
+    }
+
+    return response.status(200).json();
 }
