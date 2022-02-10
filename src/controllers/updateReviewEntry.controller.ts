f1d1b5a10b8c10a0f8c56b442f66c03c8e752b04
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-02-10 23:47:21 +0200

    RBH-9 | Setup updateReviewEntryController

--- src/controllers/updateReviewEntry.controller.ts
+++ src/controllers/updateReviewEntry.controller.ts
@@ -2,5 +2,34 @@ import { Request, Response } from "express";
 import reviewsModel from "../models/reviews.model";
 
 export async function updateReviewEntryController(request: Request, response: Response) {
-    // implement me
+    const { numberOfStars, numberOfStarsToUpdate,
+            comment, commentToUpdate,
+            postedOn, postedOnToUpdate,
+            editedAt, editedAtToUpdate,
+            author, authorToUpdate,
+        } = request.body;
+
+    const filterData = {
+        numberOfStars: numberOfStars,
+        comment: comment,
+        postedOn: postedOn,
+        editedAt: editedAt,
+        author: author
+    };
+
+    const updateData = {
+        numberOfStars: numberOfStarsToUpdate,
+        comment: commentToUpdate,
+        postedOn: postedOnToUpdate,
+        editedAt: editedAtToUpdate,
+        author: authorToUpdate
+    };
+
+    const data = await reviewsModel.findOneAndUpdate(filterData, updateData);
+
+    if (data) {
+        console.log(`Data ${data} was updated!`);
+    }
+
+    return response.status(200).json;
 }
