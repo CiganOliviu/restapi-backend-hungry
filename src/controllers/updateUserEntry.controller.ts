1676b81c6f8be27c43d2e6a78e8cebeecdb5ff9a
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-02-10 23:57:41 +0200

    RBH-9 | Create updateUserEntryController

--- src/controllers/updateUserEntry.controller.ts
+++ src/controllers/updateUserEntry.controller.ts
@@ -1,6 +1,39 @@
 import { Request, Response } from "express";
 import UsersModel from "../models/users.model";
+import usersModel from "../models/users.model";
 
 export async function updateUserEntryController(request: Request, response: Response) {
-    // implement me
+    const { username, usernameToUpdate,
+            firstName, firstNameToUpdate,
+            lastName, lastNameToUpdate,
+            email, emailToUpdate,
+            password, passwordToUpdate,
+            isAdmin, isAdminToUpdate,
+    } = request.body;
+
+    const filterData = {
+        username: username,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        isAdmin: isAdmin,
+    }
+
+    const updateData = {
+        username: usernameToUpdate,
+        firstName: firstNameToUpdate,
+        lastName: lastNameToUpdate,
+        email: emailToUpdate,
+        password: passwordToUpdate,
+        isAdmin: isAdminToUpdate,
+    }
+
+    const data = await usersModel.findOneAndUpdate(filterData, updateData);
+
+    if (data) {
+        console.log(`Data ${data} was updated!`);
+    }
+
+    return response.status(200).json();
 }
