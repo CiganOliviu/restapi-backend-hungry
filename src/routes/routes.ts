b5bd5e98127e656a20996be5a26021199bc5f30c
Catalin Ionita <catalin_ionita04@yahoo.com>
2022-03-04 01:05:02 +0200

    Setup foodCategories controller by new structure

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Express } from "express";
 import { operateCountryController } from "../controllers/countries.controller";
 import { operateReviewsController } from "../controllers/reviews.controller";
+import { operateFoodCategoriesController } from "../controllers/foodCategories.controller";
 
 export function setRoutes(app: Express) {
     app.use('/countries', operateCountryController);
     app.use('/reviews', operateReviewsController);
+    app.use('/foodCategories', operateFoodCategoriesController)
 }
